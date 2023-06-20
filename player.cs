f90aec52ec5458fd217a69b7cfd8971500549297
EmirhanKilic767 <emirhan.kilic@stu.khas.edu.tr>
2023-06-20 14:24:16 +0300

    BattleShip_Console_Game
    BattleShip console game created by C#.

---
+++ player.cs
@@ -0,0 +1,23 @@
+class Player 
+{
+    public string name;        
+    public int score;
+    
+    public Player(int inScore){//Constructor
+      
+
+        score =inScore;
+   
+      
+    }
+  
+    public Player(string inName, int inScore){//Constructor
+      
+        name= inName;
+        score =inScore;
+   
+      
+    }
+}
+
+
